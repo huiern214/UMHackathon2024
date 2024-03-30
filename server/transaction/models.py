6a70c728bcc797ff4c5f29eda88d69fa21881d6e
chuanlintneoh <chuanlin.tneoh@gmail.com>
2024-03-30 11:33:19 +0800

    Get transaction data

--- server/transaction/models.py
+++ server/transaction/models.py
@@ -1,54 +1,79 @@
 from django.db import models
-from supabase_client import supabase
+# from .supabase_client import supabase
 
 # Create your models here.
-class Transaction(models.Model):
-  transactionID = models.TextField()
-  transactionTableID = models.BigIntegerField()
-  date = models.DateField()
-  transactionDetails = models.TextField()
-  description = models.TextField()
-  category = models.TextField()
-  paymentMethod = models.TextField()
-  withdrawalAmt = models.FloatField()
-  depositAmt = models.FloatField()
+# class Transaction(models.Model):
+#   transactionID = models.AutoField(primary_key=True)
+#   transactionTableID = models.BigIntegerField()
+#   date = models.DateField()
+#   transactionDetails = models.TextField()
+#   description = models.TextField()
+#   category = models.TextField()
+#   paymentMethod = models.TextField()
+#   withdrawalAmt = models.FloatField()
+#   depositAmt = models.FloatField()
 
-  @staticmethod
-  def create_transaction(data):
-    result = supabase.table('transactions').insert(data).execute()
-    return result.data
+#   @staticmethod
+#   def create_transaction(data):
+#     result = supabase.table('Transactions').insert(data).execute()
+#     return result.data
   
-  @staticmethod
-  def get_transaaction(transactionID):
-    result = supabase.table('transactions').select('*').eq('transactionID', transactionID).execute()
-    return result.data
+#   @staticmethod
+#   def read_transaction(transactionID):
+#     result = supabase.table('Transactions').select('*').eq('transactionID', transactionID).execute()
+#     return result.data
   
-  def custom_query(sql_query):
-    return Transaction.objects.raw(sql_query)
+#   @staticmethod
+#   def update_transaction(transactionID, data):
+#     result = supabase.table('Transactions').update(data).eq('transactionID', transactionID).execute()
+#     return result.data
   
-class TransactionTable(models.Model):
-  transactionTableID = models.BigIntegerField()
-  userID = models.BigIntegerField()
-  transactionTableName = models.TextField()
+#   @staticmethod
+#   def delete_transaction(transactionID):
+#     result = supabase.table('Transactions').delete().eq('transactionID', transactionID).execute()
+#     return result.data
+  
+#   @staticmethod
+#   def get_transactions_for_table(transactionTableID):
+#     result = supabase.table('Transactions').select('*').eq('transactionTableID', transactionTableID).execute()
+#     return result.data
+  
+#   def custom_query(sql_query):
+#     return Transaction.objects.raw(sql_query)
+  
+# class TransactionTable(models.Model):
+#   transactionTableID = models.AutoField(primary_key=True)
+#   userID = models.BigIntegerField()
+#   transactionTableName = models.TextField()
+  
+#   @staticmethod
+#   def create_transaction_table(data):
+#     result = supabase.table('TransactionTables').insert(data).execute()
+#     return result.data
+  
+#   @staticmethod
+#   def read_transaction_table(transactionTableID):
+#     result = supabase.table('TransactionTables').select('*').eq('transactionTableID', transactionTableID).execute()
+#     return result.data
+  
+#   @staticmethod
+#   def update_transaction_table(transactionTableID, data):
+#     result = supabase.table('TransactionTables').update(data).eq('transactionTableID', transactionTableID).execute()
+#     return result.data
+  
+#   @staticmethod
+#   def delete_transaction_table(transactionTableID):
+#     result = supabase.table('TransactionTables').delete().eq('transactionTableID', transactionTableID).execute()
+#     return result.data
+  
+#   @staticmethod
+#   def get_transaction_tables_for_user(userID):
+#     result = supabase.table('TransactionTables').select('transactionTableID', 'transactionTableName').eq('userID', userID).execute()
+#     return result.data
+  
+#   def custom_query(sql_query):
+#     return TransactionTable.objects.raw(sql_query)
 
-  @staticmethod
-  def get_transaction_tables_for_user(userID):
-    result = supabase.table('transaction_tables').select('transactionTableID', 'transactionTableName').eq('userID', userID).execute()
-    return result.data
-  
-  @staticmethod
-  def create_transaction_table(data):
-    result = supabase.table('transaction_tables').insert(data).execute()
-    return result.data
-  
-  @staticmethod
-  def get_transaction_table(transactionTableID):
-    result = supabase.table('transaction_tables').select('*').eq('transactionTableID', transactionTableID).execute()
-    return result.data
-  
-  def custom_query(sql_query):
-    return TransactionTable.objects.raw(sql_query)
-
-# tester
-if __name__ == '__main__':
-  print(TransactionTable.custom_query("SELECT * FROM transaction_transactiontable WHERE transactionTableName = 'Test Table'"))
+# # tester
+# if __name__ == '__main__':
+#   print(TransactionTable.custom_query("SELECT * FROM transaction_transactiontable WHERE transactionTableName = 'Test Table'"))
